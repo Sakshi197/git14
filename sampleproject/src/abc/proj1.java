60b750cec4ed1c541a0f76d17c6f5fe58d09b68f
Sakshi197 <sakshisaini17jan@gmail.com>
2024-11-14 15:10:47 +0530

    commit

---
+++ sampleproject/src/abc/proj1.java
@@ -0,0 +1,11 @@
+package abc;
+
+public class proj1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to alliance");
+		System.out.println("Welcome to cse gen b");
+
+	}
+
+}
